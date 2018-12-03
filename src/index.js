195fd703e0966902a15de618dac94b7e8e878164
Andrzej Szmajnta <andrzej.szmajnta.dev@gmail.com>
2018-12-03 16:12:29 +0100

    Add state control to filters

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 import './assets/css/animate.min.css'
 import './assets/css/demo.css'
 import './assets/css/pe-icon-7-stroke.css'
-import './assets/sass/light-bootstrap-dashboard.css?v=1.2.0'
+import './assets/sass/light-bootstrap-dashboard.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
--- src/views/Reports/Reports.jsx
+++ src/views/Reports/Reports.jsx
@@ -13,88 +13,97 @@ import './reports.scss'
 const rateOptions = [
 	{
 		value: 0,
-		text: 'Never'
+		text: 'Freeze'
 	},
 	{
-		value: 10,
-		text: '10 seconds'
+		value: 30,
+		text: '30 seconds'
 	},
 	{
-		value: 15,
-		text: '15 seconds'
+		value: 60,
+		text: 'Every minute'
 	}
 ]
 
 class Reports extends Component {
 	state = {
-		rate: { value: 0, text: 'Never' }
+		rate: { value: 0, text: 'Never' },
+		filter: 0
 	}
 
 	handleChangeRefreshRate = rate => {
 		this.setState({ rate })
 	}
 
+	changeFilter = val => {
+		this.setState({
+			filter: val
+		})
+	}
+
 	renderRefreshRateDropdown = () => (
-		<Dropdown id={'dropdown-refresh-rate'}>
-			<Dropdown.Toggle id="dropdown-basic">
-				{this.state.rate.text}
-			</Dropdown.Toggle>
+		<div className={'reports__refresh-container'}>
+			<p>Refresh: </p>
+			<Dropdown id={'dropdown-refresh-rate'}>
+				<Dropdown.Toggle id="dropdown-basic">
+					{this.state.rate.text}
+				</Dropdown.Toggle>
 
-			<Dropdown.Menu>
-				{rateOptions.map(rate => (
-					<MenuItem
-						active={rate.value === this.state.rate.value}
-						key={`rate-index-item-${rate.value}`}
-						onSelect={() => this.handleChangeRefreshRate(rate)}
-					>
-						{rate.text}
-					</MenuItem>
-				))}
-			</Dropdown.Menu>
-		</Dropdown>
+				<Dropdown.Menu>
+					{rateOptions.map(rate => (
+						<MenuItem
+							active={rate.value === this.state.rate.value}
+							key={`rate-index-item-${rate.value}`}
+							onSelect={() => this.handleChangeRefreshRate(rate)}
+						>
+							{rate.text}
+						</MenuItem>
+					))}
+				</Dropdown.Menu>
+			</Dropdown>
+		</div>
 	)
 
-	renderFilterButtons = () => (
-		<ButtonGroup>
-			<Button bsStyle="primary">All</Button>
-			<Button bsStyle="primary">Allowed</Button>
-			<Button bsStyle="primary">Alert</Button>
-		</ButtonGroup>
-	)
+	renderFilterButtons = () => {
+		const { filter } = this.state
+		return (
+			<ButtonGroup className={'reports__filter-buttons'}>
+				<Button
+					onClick={() => this.changeFilter(0)}
+					bsStyle={filter === 0 ? 'primary' : 'default'}
+				>
+					All
+				</Button>
+				<Button
+					onClick={() => this.changeFilter(1)}
+					bsStyle={filter === 1 ? 'primary' : 'default'}
+				>
+					Allowed
+				</Button>
+				<Button
+					onClick={() => this.changeFilter(2)}
+					bsStyle={filter === 2 ? 'primary' : 'default'}
+				>
+					Alert
+				</Button>
+			</ButtonGroup>
+		)
+	}
 
 	render() {
 		return (
-			<div className="content report">
+			<div className="content reports">
 				<Grid fluid>
 					<Row>{/*<SearchComponent/>*/}</Row>
-					<Row
-						style={{
-							display: 'flex',
-							flexDirection: 'row',
-							alignItems: 'center'
-						}}
-					>
-						<Col
-							md={8}
-							style={{
-								display: 'flex',
-								flexDirection: 'row',
-								alignItems: 'center'
-							}}
-						>
+					<Row>
+						<Col xs={12} sm={3}>
 							<p>Realtime View</p>
 						</Col>
-						<Col
-							md={4}
-							style={{
-								display: 'flex',
-								flexDirection: 'row',
-								alignItems: 'center'
-							}}
-						>
-							<p>Refresh: </p>
-							{this.renderRefreshRateDropdown()}
-							{this.renderFilterButtons()}
+						<Col sm={9} xs={12}>
+							<div className={'right-container'}>
+								{this.renderRefreshRateDropdown()}
+								{this.renderFilterButtons()}
+							</div>
 						</Col>
 					</Row>
 				</Grid>
