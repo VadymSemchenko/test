809a6af3c3e94f6a4f3219f193ccf3134749f9f8
Andrzej Szmajnta <andrzej.szmajnta.dev@gmail.com>
2018-12-06 20:11:11 +0100

    Add more icons

--- src/views/Reports/components/ReportsTableItem/ReportTableItem.js
+++ src/views/Reports/components/ReportsTableItem/ReportTableItem.js
@@ -8,14 +8,18 @@ import ThreatIcon from '../../../../assets/img/PNG/Acreto_Icon 18.png'
 import TerminatedIcon from '../../../../assets/img/PNG/Acreto_Icon 19.png'
 import ActiveIcon from '../../../../assets/img/PNG/Acreto_Icon 20.png'
 import AllowIcon from '../../../../assets/img/PNG/Acreto_Icon 23.png'
+import ChainIcon from '../../../../assets/img/PNG/Acreto_Icon 24.png'
+import CompletedIcon from '../../../../assets/img/PNG/Acreto_Icon 27.png'
+import TimetoutIcon from '../../../../assets/img/PNG/Acreto_Icon 28.png'
+import PolicyIcon from '../../../../assets/img/PNG/policy_icon.png'
 
 import './reports-table-item.scss'
 
 const STATUS = [
 	{ slug: 'active', name: 'Active', icon: ActiveIcon },
-	{ slug: 'completed', icon: '', name: 'Completed' },
+	{ slug: 'completed', icon: CompletedIcon, name: 'Completed' },
 	{ slug: 'terminated', icon: TerminatedIcon, name: 'Terminated' },
-	{ slug: 'timeout', icon: '', name: 'Timed out' }
+	{ slug: 'timeout', icon: TimetoutIcon, name: 'Timed out' }
 ]
 
 export default function ReportsTableItem({ data }) {
@@ -24,6 +28,8 @@ export default function ReportsTableItem({ data }) {
 		switch (action) {
 			case 'Allow':
 				return AllowIcon
+			case 'URL':
+				return ChainIcon
 			default:
 				return null
 		}
@@ -36,7 +42,7 @@ export default function ReportsTableItem({ data }) {
 					alt={'arrow-icon'}
 					className={'small-icon'}
 				/>
-				<img alt={'policy-icon'} className={'small-icon'} />
+				<img src={PolicyIcon} alt={'policy-icon'} className={'small-icon'} />
 				<div>
 					<p className={'medium'}>{moment(data.date).format('MM/DD/YYYY')}</p>
 					<p className={'medium'}>{moment(data.date).format('HH:mm:ss')}</p>
