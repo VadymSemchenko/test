25ebcfbbc27b358e6ca00c0666b3405e3aedab96
Andrzej Szmajnta <andrzej.szmajnta.dev@gmail.com>
2018-12-12 12:05:15 +0100

    Ecosystem item component

--- src/views/Ecosystems/scenario-actions.js
+++ src/views/Ecosystems/scenario-actions.js
@@ -1,4 +1,8 @@
-// export function fetchEcosystems() {
+export function fetchEcosystems() {
+	return dispatch => {
+		return dispatch
+	}
+}
 // 	return dispatch => {
 // 		try {
 //
@@ -6,7 +10,11 @@
 // 	}
 // }
 //
-// export function openEcosystem(ecosystem) {
+export function openEcosystem(ecosystem) {
+	return dispatch => {
+		return { ecosystem, dispatch }
+	}
+}
 // 	return dispatch => {
 // 		try {
 // 		} catch (err) {}
