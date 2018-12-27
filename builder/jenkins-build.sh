2382ec50259ea4ab4a1fb4ccc819145cfa67c137
Lukasz Klimek <lukasz@softinn.eu>
2018-12-27 12:17:05 +0100

    build scripts for jenkins

---
+++ builder/jenkins-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd $WORKSPACE
+
+$WORKSPACE/builder/build -i ubuntu-deb-builder:18.04 \
+    -o $WORKSPACE/output \
+    -p wedge \
+    $WORKSPACE/
+
+cd $WORKSPACE/output
+sha256sum * > SHA256SUMS
