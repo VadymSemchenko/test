72c6884c2edfbc712de3c5adc2c6dd98ae6914b4
Andrzej Szmajnta <andrzej.szmajnta.dev@gmail.com>
2019-01-21 16:10:19 +0100

    Save token in local storage

--- src/store/auth/reducer.js
+++ src/store/auth/reducer.js
@@ -1,8 +1,9 @@
 import { LOGIN_FAILURE, LOGIN_SUCCESS } from './action-types'
+const LOCAL_ACCESS_TOKEN_KEY = 'wedge_access_token'
 
 const initialState = {
-	isAuthenticated: false,
-	token: ''
+	isAuthenticated: localStorage.getItem(LOCAL_ACCESS_TOKEN_KEY) !== null,
+	token: localStorage.getItem(LOCAL_ACCESS_TOKEN_KEY)
 }
 
 export function authReducer(state = initialState, { type, payload }) {
@@ -14,6 +15,7 @@ export function authReducer(state = initialState, { type, payload }) {
 				token: ''
 			}
 		case LOGIN_SUCCESS:
+			localStorage.setItem(LOCAL_ACCESS_TOKEN_KEY, payload.accessToken)
 			return {
 				...state,
 				isAuthenticated: true,
