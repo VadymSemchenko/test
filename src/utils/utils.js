70782a1cb377dd47c9185945ab7cc998ce51e07b
Andrzej Szmajnta <andrzej.szmajnta.dev@gmail.com>
2019-02-02 00:07:34 +0100

    Read customer from token, move method to utils

---
+++ src/utils/utils.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+import unique from 'lodash/uniqBy'
+
+export function extractCustomers(roles) {
+	const split = roles.split(' ')
+	const customers = split.map(part => {
+		return {
+			name: part.split('.')[0],
+			id: part.split('.')[1]
+		}
+	})
+
+	return unique(customers, 'id')
+}
+
+export function extractCustomerFromToken(token) {
+	const decodedToken = jwt.decode(token, { json: true })
+	return extractCustomers(decodedToken.roles)
+}
