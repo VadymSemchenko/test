2783e58102a403121b2eeabee6cbe10dffdbea31
Vadym Semchenko <v.m.semchenko.kh.ua@gmail.com>
2019-02-25 09:52:58 +0200

    added reducer handling with redux-actions

--- src/store/user/reducer.js
+++ src/store/user/reducer.js
@@ -1,3 +1,7 @@
+// import { set } from 'lodash/fp'
+import { assign } from 'lodash'
+import { handleActions } from 'redux-actions'
+
 import {
 	SET_USER,
 	SET_EMAIL,
@@ -6,7 +10,6 @@ import {
 	SET_ERROR,
 	CLEAR_ERROR
 } from './actionTypes'
-import { set } from 'lodash/fp'
 
 const initialState = {
 	email: '',
@@ -18,21 +21,33 @@ const initialState = {
 	error: ''
 }
 
-export const userReducer = (state = initialState, { type, payload }) => {
-	switch (type) {
-		case SET_USER:
-			return payload
-		case SET_EMAIL:
-			return set('email', payload, state)
-		case START_LOADING:
-			return set('isLoading', true, state)
-		case FINISH_LOADING:
-			return set('isLoading', false, state)
-		case SET_ERROR:
-			return set('error', payload, state)
-		case CLEAR_ERROR:
-			return set('error', '', state)
-		default:
-			return state
-	}
-}
+// export const userReducer = (state = initialState, { type, payload }) => {
+// 	switch (type) {
+// 		case SET_USER:
+// 			return payload
+// 		case SET_EMAIL:
+// 			return set('email', payload, state)
+// 		case START_LOADING:
+// 			return set('isLoading', true, state)
+// 		case FINISH_LOADING:
+// 			return set('isLoading', false, state)
+// 		case SET_ERROR:
+// 			return set('error', payload, state)
+// 		case CLEAR_ERROR:
+// 			return set('error', '', state)
+// 		default:
+// 			return state
+// 	}
+// }
+
+export const userReducer = handleActions(
+	{
+		[SET_USER]: (_, { payload }) => payload,
+		[SET_EMAIL]: (state, { payload: email }) => assign({}, state, { email }),
+		[START_LOADING]: state => assign({}, state, { isLoading: true }),
+		[FINISH_LOADING]: state => assign({}, state, { isLoading: true }),
+		[SET_ERROR]: (state, { payload: error }) => assign({}, state, { error }),
+		[CLEAR_ERROR]: state => assign({}, state, { error: '' })
+	},
+	initialState
+)
